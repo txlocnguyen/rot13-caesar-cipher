f104997eeba876d1ab8ec7a59d7ac3c68597dbfd
Loc Nguyen <txlocnguyen@gmail.com>
2022-02-06 21:45:58 -0600

    finishing up

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ function rot13(str) {
     const after = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
     return str.replace(/[a-z]/ig, letter => after[before.indexOf(letter)]);
 }
+
+function printAnswer(yourStr, answer) {
+    let result = document.getElementById("result");
+    result.innerHTML = `With ROT13, <span style="color:green;">${yourStr}</span> becomes <span style="color:red;">${answer}</span>`
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    let input = document.getElementById("input");
+    let submit = document.getElementById("submit");
+    submit.addEventListener("click", (event) => {
+        event.preventDefault();
+        if (input.value === "") {
+            alert("You did not enter any text. Please try again.");
+        } else {
+            printAnswer(input.value, rot13(input.value));
+            input.value = "";
+        }
+    })
+})
